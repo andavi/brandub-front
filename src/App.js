194733e79feef3a49b978d63dee4bc5c268f7b45
Andrew Davis <andrew.bradley.davis@gmail.com>
2021-05-18 12:38:44 -0700

    tedious prereqs: logo, favicon, etc

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './modernClover.svg';
+import logo from './celtic-clover.svg';
 import './App.css';
 
 function App() {
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Under Construction
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/andavi/brandub-front"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Brandub
         </a>
       </header>
     </div>
