32ebb372843aefa8723931f84e9f02b7c0e397e1
Andrew Davis <andrew.bradley.davis@gmail.com>
2021-05-30 10:17:01 -0700

    went back to get original page to use as login, made mistakes, going back to rendering game page - will to the routing later

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { render } from "react-dom";
-import { Client } from "boardgame.io/react";
-import Brandub from "./game";
-import Board from "./board";
+// import { Client } from "boardgame.io/react";
+// import Brandub from "./Game";
+// import Board from "./Board";
+import Welcome from "./Welcome";
 
-const App = Client({
-  game: Brandub,
-  board: Board
-});
+// eslint-disable-next-line no-unused-vars
+// const App = Client({
+//   game: Brandub,
+//   board: Board
+// });
 
-render(<App />, document.getElementById("root"));
+render(<Welcome />, document.getElementById("root"));
